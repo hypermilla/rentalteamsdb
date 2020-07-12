e973ec6ea1fad8deaf2ad844a2196f59ebe29b57
hypermilla <hypermilla@protonmail.com>
2020-07-12 20:00:29 -0300

    new header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     
     return (
-        <div className="header navbar bg-darker">
-			<Link to="/">
-            	<h1>RotomBox</h1>
-			</Link>
-
+        <div className="header bg-darker">
 			<Link to="/newteam" type="button" className="new-team-btn btn btn-primary">
-                Add New Rental Team
-            </Link>
+					Add New Rental Team
+			</Link>
+			<div className="logo">
+				<img align="center" src={require("../img/logo.png")} />
+			</div>
+			<h1><spam className="rotom">Rotom</spam><spam className="box">Box</spam></h1>
         </div>
     );
 }
